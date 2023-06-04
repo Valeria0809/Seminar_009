da4344dbfc4a10a309e1e50cf28fb75e3e851033
VALERIA <valeri.tereshenkova@icloud.com>
2023-06-04 19:59:34 +0300

    Task 63

---
+++ task63/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 63: Задайте значение N. Напишите программу, 
+// которая выведет все натуральные числа в промежутке от 1 до N.
+// N = 5 -> "1, 2, 3, 4, 5"
+// N = 6 -> "1, 2, 3, 4, 5, 6"
+
+int ReadInt(string text)
+{
+    System.Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void Number(int N)
+{
+    if(N < 1) return;
+    Number(N - 1);
+    System.Console.Write(N + " ");
+}
+
+Number(ReadInt("Введите значение N: "));
